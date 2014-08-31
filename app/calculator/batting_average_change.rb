7c9280cf8108125f747963ec05e70b03821f5cf8
masonjm <masonjm@gmail.com>
2014-08-30 23:01:37 -0700

    Fixed batting average handling for players who only played one year

--- app/calculator/batting_average_change.rb
+++ app/calculator/batting_average_change.rb
@@ -1,21 +1,29 @@
 class BattingAverageChange
-  class Result
-    attr_reader :player, :change
+  class MostImprovedResult
+    attr_reader :from_stat, :to_stat
     
-    def initialize(player: nil, change: nil)
-      @player = player
-      @change = change
+    def initialize(from_stat, to_stat)
+      @from_stat = from_stat
+      @to_stat = to_stat
     end
     
     def to_s
-      "#{player} " +
+      "#{player} had the most improved batting average from #{from_stat.year} to #{to_stat.year}: " +
         if change > 0
-          "improved by #{change}"
+          "improved from #{from_stat.batting_average} to #{to_stat.batting_average}"
         elsif change < 0
-          "declined by #{change}"
+          "declined from #{from_stat.batting_average} to #{to_stat.batting_average}"
         else
-          "did not change"
+          "no change"
         end
+    end
+    
+    def player
+      @player ||= @from_stat.player
+    end
+    
+    def change
+      @change ||= @to_stat.batting_average - @from_stat.batting_average
     end
   end
   
@@ -28,23 +36,33 @@ class BattingAverageChange
   end
   
   def most_improved
-    player = nil
-    most_changed = 0
-    players_with_batting_averages.each_slice(2) do |stats|
+    most_improved = 0
+    from_stat = nil
+    to_stat = nil
+    
+    stats_by_player_id.each do |player_id, stats|
+      next if stats.length != 2 # Skip players who didn't play both years
       from = stats.first
       to = stats.last
-      player_change = to.batting_average - from.batting_average
-      if player_change > most_changed
-        player = from.player
-        most_changed = player_change
+      change = to.batting_average - from.batting_average
+      if change > most_improved
+        most_improved = change
+        from_stat = from
+        to_stat = to
       end
     end
-    Result.new(player: player, change: most_changed)
+    
+    MostImprovedResult.new(from_stat, to_stat)
   end
   
   private
   
-  def players_with_batting_averages
-    HittingStat.for_years(from_year, to_year).where(["at_bats >= ?", minimum_at_bats]).order("player_id, year")
+  def stats_by_player_id
+    HittingStat.for_years(from_year, to_year).where(["at_bats >= ?", minimum_at_bats]).order("player_id, year").inject({}) do |stats, stat|
+      player = stat.player_id
+      stats[player] ||= []
+      stats[player] << stat
+      stats
+    end
   end
 end
